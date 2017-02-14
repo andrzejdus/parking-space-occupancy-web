64f902d0b4414895dec18d5e6cd347fba9951d21
Andrzej Duś <andrzej.dus@gmail.com>
2017-02-14 20:20:37 +0100

    Project is now based on deployd

---
+++ resources/calibration/beforerequest.js
@@ -0,0 +1,7 @@
+ctx.done({
+  "message": "Calibration data sent successfully.",
+  "data": {
+    "distanceHysteresis": 25,
+    "splitDistance": 150
+  }
+}, { ok: true } );
--- server.js
+++ server.js
@@ -1,75 +1,22 @@
-'use strict';
+let deployd = require('deployd');
 
-const Hapi = require('hapi');
-const Joi = require('joi');
-
-const server = new Hapi.Server();
-server.connection({
-    port: process.env.PORT || 8000
+let server = deployd({
+  port: process.env.PORT || 8000,
+  env: 'production',
+  db: {
+    connectionString: process.env.MONGODB_URI || 'mongodb://heroku_xd5vwxrx:l57ba6l0qaufndgcphmt2g06vq@ds149489.mlab.com:49489/heroku_xd5vwxrx'
+  }
 });
 
-server.route({
-    method: 'POST',
-    path:'/measurement',
-    handler: function (request, reply) {
-        console.log('POST /measurement');
-        console.log(request.payload);
-        return reply({
-            statusCode: 200,
-            message: 'Measurement saved successfully.'
-        });
-    },
-    config: {
-        validate: {
-            payload: {
-                macAddress: Joi.string().required(),
-                distance: Joi.number().required().integer(),
-            }
-        }
-    }
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening");
 });
 
-server.route({
-    method: 'POST',
-    path:'/occupied',
-    handler: function (request, reply) {
-        console.log('POST /occupied');
-        console.log(request.payload);
-        return reply({
-            statusCode: 200,
-            message: 'Spot occupation saved successfully.'
-        });
-    },
-    config: {
-        validate: {
-            payload: {
-                macAddress: Joi.string().required(),
-                isOccupied: Joi.boolean().required()
-            }
-        }
-    }
-});
-
-server.route({
-    method: 'GET',
-    path:'/calibration',
-    handler: function (request, reply) {
-        console.log('GET /calibration');
-        return reply({
-            statusCode: 200,
-            message: 'Calibration data sent successfully.',
-            data: {
-                distanceHysteresis: 25,
-                splitDistance: 150
-            }
-        });
-    }
-});
-
-server.start((err) => {
-    if (err) {
-        throw err;
-    }
-
-    console.log('Server running at:', server.info.uri);
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
 });
